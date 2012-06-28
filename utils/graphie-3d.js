f574deb18fe1ee74099d8d9923b8fe596a8e8beb
Emily Eisenberg <xymostech@gmail.com>
2012-06-28 14:28:12 -0700

    Do some commenting

--- utils/graphie-3d.js
+++ utils/graphie-3d.js
@@ -1,4 +1,6 @@
 $.extend(KhanUtil, {
+    // make a 3d object, which holds the vertices,
+    //   faces, and perspective of an object
     makeObject: function(verts) {
         var object = {
             verts: verts,
@@ -10,6 +12,7 @@ $.extend(KhanUtil, {
             faces: []
         };
 
+        // find the length of a 3d vector
         var vectorLength = function(v) {
             return Math.sqrt(v[0] * v[0] +
                              v[1] * v[1] +
@@ -18,6 +21,7 @@ $.extend(KhanUtil, {
 
         var graph = KhanUtil.currentGraph;
 
+        // set and offset the camera pos
         object.offsetPos = function(offset) {
             this.perspective.pos[0] += offset[0];
             this.perspective.pos[1] += offset[1];
@@ -28,6 +32,7 @@ $.extend(KhanUtil, {
             this.perspective.pos = pos.slice();
         };
 
+        // set and offset the camera angle
         object.offsetAngle = function(offset) {
             this.perspective.angle[0] += offset[0];
             this.perspective.angle[1] += offset[1];
@@ -38,6 +43,8 @@ $.extend(KhanUtil, {
             this.perspective.angle = angle.slice();
         };
 
+        // perform the perspective transformation stored in
+        //   object.perspective on a 3d point
         object.doPerspective = function(pt) {
             var p = [];
 
@@ -63,6 +70,8 @@ $.extend(KhanUtil, {
             return p;
         };
 
+        // perform the perspective transformation and then project
+        //   the 3d point onto a 2d screen
         object.doProjection = function(pt) {
             var p = this.doPerspective(pt);
 
@@ -74,12 +83,15 @@ $.extend(KhanUtil, {
             return [x1, y1];
         };
 
+        // add a face to the object, with verts being indices of the
+        //   object.verts array
         object.addFace = function(options) {
             var face = $.extend(true, {
                 verts: [],
                 color: "black"
             }, options);
 
+            // compute the normal of a face
             face.normal = function() {
                 var a = object.doPerspective(object.verts[this.verts[0]]);
                 var b = object.doPerspective(object.verts[this.verts[1]]);
@@ -99,12 +111,14 @@ $.extend(KhanUtil, {
                 return _.map(normal, function(e) { return e / length; });
             };
 
+            // find the array of the projected points of the face
             face.mappedVerts = function() {
                 return _.map(this.verts, function(v) {
                     return object.doProjection(object.verts[v]);
                 });
             };
 
+            // create a path of the face
             face.path = function() {
                 return graph.path(
                     face.mappedVerts(),
@@ -117,15 +131,20 @@ $.extend(KhanUtil, {
             return this;
         };
 
+        // draw the object, performing backface culling to ensure
+        //   faces don't intersect each other
         object.draw = function() {
+            // sort the faces by how much the faces point backwards
             var sortedFaces = _.sortBy(this.faces, function(face) {
                 return face.normal()[2];
             });
 
+            // filter out the faces that don't face forwards
             var drawFaces = _.filter(sortedFaces, function(face) {
                 return face.normal()[2] > 0;
             });
 
+            // draw each of the faces, and store it in a raphael set
             var image = graph.raphael.set();
             _.each(drawFaces, function(face) {
                 image.push(face.path());
@@ -133,15 +152,25 @@ $.extend(KhanUtil, {
             return image;
         };
 
+        // a list of the current and next frame
+        // each time a new one is created, the old one is
+        // removed later to avoid fast flickering
         object.images = [graph.raphael.set()];
+
+        // whether or not an image is pending for deletion,
+        // in which case we shouldn't draw again
         object.drawPending = false;
 
+        // do the full double-buffered drawing
         object.doDraw = function() {
+            // only draw if we don't have a second frame waiting
             if (!this.drawPending) {
                 this.drawPending = true;
 
+                // do the drawing, and store the new frame
                 this.images.push(this.draw());
 
+                // defer removing the old frame
                 _.defer(function() {
                     object.images.shift().remove();
                     object.drawPending = false;

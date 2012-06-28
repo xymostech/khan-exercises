0b52d7f0104a469d9c3e74f40762389d745c54a0
Emily Eisenberg <xymostech@gmail.com>
2012-06-28 14:33:59 -0700

    Add matrix multiplication functions

--- utils/matrix.js
+++ utils/matrix.js
@@ -11,5 +11,37 @@ $.extend(KhanUtil, {
                 return deepZipWith.apply(this, [depth - 1, fn].concat(els));
             });
         }
+    },
+
+    // add matrix properties to a 2d matrix
+    //   currently only rows and columns
+    makeMatrix: function(m) {
+        m.r = m.length;
+        m.c = m[0].length;
+
+        return m;
+    },
+
+    // multiply two matrices
+    matrixMult: function(a, b) {
+        var c = [];
+        // create the new matrix
+        for (var i = 0; i < a.r; ++i) {
+            c.push([]);
+        }
+
+        // perform the multiply
+        for (var i = 0; i < a.r; ++i) {
+            for (var j = 0; j < b.c; ++j) {
+                var temp = 0;
+                for (var k = 0; k < a.c; ++k) {
+                    temp += a[i][k] * b[k][j];
+                }
+                c[i][j] = temp;
+            }
+        }
+
+        // add matrix properties to the result
+        return makeMatrix(c);
     }
 });
